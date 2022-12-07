90d0e7c987265466072127933d8384504d01ed4c
Luna D <luna@nighty.cloud>
2022-12-07 15:22:54 +0100

    move composable

--- app/src/main/java/com/nighty/testbed/MainActivity.kt
+++ app/src/main/java/com/nighty/testbed/MainActivity.kt
@@ -4,15 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.nighty.testbed.ui.theme.TestbedTheme
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
@@ -50,50 +46,6 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     RandomText()
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun RandomText() {
-        var inputTextField by remember { mutableStateOf("1") }
-        var bodyText by remember { mutableStateOf("(text will be displayed here once fetched)") }
-
-        Column(
-            verticalArrangement = Arrangement.Top,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            OutlinedTextField(
-                value = inputTextField,
-                onValueChange = { inputTextField = it },
-                label = { Text("Amount of paragraphs") }
-            )
-            Button(
-                onClick = {
-                    viewModel.loadLoremIpsum(
-                        inputTextField.toInt(),
-                        onFinish = { response ->
-                            bodyText = response
-                        })
-                },
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxWidth()
-                    .padding(16.dp)
-                    .border(1.dp, Color.Red)
-            ) {
-                Text(text = "Load")
-            }
-
-            val state = viewModel.stateFlow.collectAsState().value
-
-            when (val s = state) {
-                MainViewState.Loading -> {
-                    CircularProgressIndicator()
-                }
-                is MainViewState.Ready -> {
-                    Text(text = s.loremIpsum)
                 }
             }
         }
---
+++ app/src/main/java/com/nighty/testbed/RandomText.kt
@@ -0,0 +1,61 @@
+package com.nighty.testbed
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun RandomText(viewModel: MainViewModel = viewModel()) {
+    var inputTextField by remember { mutableStateOf("1") }
+    var bodyText by remember { mutableStateOf("(text will be displayed here once fetched)") }
+
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            value = inputTextField,
+            onValueChange = { inputTextField = it },
+            label = { Text("Amount of paragraphs") }
+        )
+        Button(
+            onClick = {
+                viewModel.loadLoremIpsum(
+                    inputTextField.toInt(),
+                    onFinish = { response ->
+                        bodyText = response
+                    })
+            },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .padding(16.dp)
+                .border(1.dp, Color.Red)
+        ) {
+            Text(text = "Load")
+        }
+
+        val state = viewModel.stateFlow.collectAsState().value
+
+        when (val s = state) {
+            MainViewState.Loading -> {
+                CircularProgressIndicator()
+            }
+            is MainViewState.Ready -> {
+                Text(text = s.loremIpsum)
+            }
+        }
+    }
+}
